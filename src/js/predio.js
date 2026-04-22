983e993eeb85ac4322b157363152a3ad7a651b51
Alexandre Damaso <alexandredamasocosta@gmail.com>
2026-04-21 22:19:32 -0300

    feito indentação nos arquivos

--- src/js/predio.js
+++ src/js/predio.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
 
    // Criando classe com numero de andares podendo alterar número de andares
    function criarClasse() {
@@ -44,32 +44,32 @@
 
       valorNumeroDeAndares.forEach(andares => {
          const qtdDeAndares = +andares.getAttribute('numero-de-andares')
-         for(i = qtdDeAndares; i > 0; i--) {
+         for (i = qtdDeAndares; i > 0; i--) {
             andares.appendChild(criarAndar(i))
          }
          andares.appendChild(criarTerreo())
       })
    }
-   
+
    // Criar o Elevador
    function criarElevador() {
       const poco = document.querySelector('.poco')
-      
+
       const elevador = document.createElement('div')
       elevador.classList.add('elevador')
-      
+
       elevador.style.height = tamanhoElevador() + 'px'
-      
+
       poco.appendChild(elevador)
    }
-   
+
    // Definir tamanho do elevador
    function tamanhoElevador() {
       const terreo = document.querySelector('.terreo')
       let alturaDoElevador = terreo.offsetHeight
       return alturaDoElevador
    }
-   
+
    // Mover Elevador
    function moverElevador(andar) {
       const elevador = document.querySelector('.elevador')
@@ -78,41 +78,41 @@
 
       elevador.style.bottom = elevadorIrPara + 'px'
    }
-   
+
    // Mudar o mostrador
    function atualizarMostrador(destino, localizacaoAtual) {
-         const mostrador = document.querySelector('.mostrador')
-         
-         if (localizacaoAtual < destino) {
-            mostrador.innerHTML = "Subindo"
-         } else if (localizacaoAtual > destino) {
-            mostrador.innerHTML = "Descendo"
-         } else {
-            mostrador.innerHTML = "Parado"
-         }
-         
-         let mostrarAndar = setInterval(() => {
-            let localizacaoAndar = (destino == 0 ? 'Térreo' : `${destino}º andar`)
-            mostrador.innerHTML = localizacaoAndar
-            clearInterval(mostrarAndar)
-         }, 2000)
-      }
-      
-      // Escolher Andar
-      function escolherAndar() {
-         const botoes = document.querySelectorAll('[destino]')
+      const mostrador = document.querySelector('.mostrador')
 
-         let localizacaoAtual = 0
-         botoes.forEach(botao => {
-            const destino = +botao.getAttribute('destino')
-            botao.onclick = function () {
-               moverElevador(destino)
-               atualizarMostrador(destino, localizacaoAtual)
-               localizacaoAtual = destino
-            }
-         })
+      if (localizacaoAtual < destino) {
+         mostrador.innerHTML = "Subindo"
+      } else if (localizacaoAtual > destino) {
+         mostrador.innerHTML = "Descendo"
+      } else {
+         mostrador.innerHTML = "Parado"
       }
-   
+
+      let mostrarAndar = setInterval(() => {
+         let localizacaoAndar = (destino == 0 ? 'Térreo' : `${destino}º andar`)
+         mostrador.innerHTML = localizacaoAndar
+         clearInterval(mostrarAndar)
+      }, 2000)
+   }
+
+   // Escolher Andar
+   function escolherAndar() {
+      const botoes = document.querySelectorAll('[destino]')
+
+      let localizacaoAtual = 0
+      botoes.forEach(botao => {
+         const destino = +botao.getAttribute('destino')
+         botao.onclick = function () {
+            moverElevador(destino)
+            atualizarMostrador(destino, localizacaoAtual)
+            localizacaoAtual = destino
+         }
+      })
+   }
+
    criarClasse()
    criarAndares()
    criarElevador()
--- src/js/rua.js
+++ src/js/rua.js
@@ -1,11 +1,15 @@
-(function() {
+(function () {
    function criarFaixas() {
       const valorNumeroDeFaixas = document.querySelectorAll('[numeroDeFaixas]')
+
       valorNumeroDeFaixas.forEach(faixas => {
          const qtdDeFaixas = +faixas.getAttribute('numeroDeFaixas')
-         for(i = 0; i <= qtdDeFaixas; i++) {
+
+         for (i = 0; i <= qtdDeFaixas; i++) {
             const criarFaixa = document.createElement('div')
+
             criarFaixa.classList.add('faixa')
+
             faixas.appendChild(criarFaixa)
          }
       })
